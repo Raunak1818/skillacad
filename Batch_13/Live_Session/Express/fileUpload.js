1b62e47c50ce657b0e20b187780bdc5fff842ac0
Raunak Raj Jaiswal <raunakrajjaiswal@gmail.com>
2024-05-18 12:49:26 +0530

    modified & new

--- Batch_13/Live_Session/Express/fileUpload.js
+++ Batch_13/Live_Session/Express/fileUpload.js
@@ -4,10 +4,27 @@ const upload=require('express-fileupload');
 
 app.use(upload());
 
-app.get('./',(req,res)=>{
-    res.send(__dirname+'/index.html')
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
 })
 
-app.listen(3400,()=>{
+app.post('/',(req,res)=>{
+    console.log('inside pst');
+    if(req.files){
+        var file=req.files.file;
+        var filename=file.name;
+        console.log(file);
+        file.mv('./uploads'+filename,function(err){
+            if(err){
+                res.send(err.message)
+            }
+            else{
+                res.send('file has been uploaded')
+            }
+        })
+    }
+})
+
+app.listen(3100,()=>{
     console.log('server is listening');
 })
